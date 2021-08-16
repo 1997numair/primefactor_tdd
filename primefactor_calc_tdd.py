efba9a04f37520340849a678df8f8e4c9cee95b2
numairnakhwa <40811628+numairnakhwa@users.noreply.github.com>
2021-08-16 18:50:28 +0530

    11th commit-showing correct output for input-"4"

--- primefactor_calc_tdd.py
+++ primefactor_calc_tdd.py
@@ -6,6 +6,12 @@ def is_prime(PorC):
 def find_primes(user_no):
     if(is_prime(user_no)):
         return user_no
+    factor_list=[]
+    for i in range(2,user_no):
+        if(user_no % i==0):
+            factor_list.append(i)
+    return factor_list
+
 _primeno_of=4
 find_primes(_primeno_of)
 print(find_primes(_primeno_of))
