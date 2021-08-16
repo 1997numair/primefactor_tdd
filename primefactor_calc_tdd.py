a73abc02a50ba9607db8bd023ff3b36ae181f760
numairnakhwa <40811628+numairnakhwa@users.noreply.github.com>
2021-08-16 18:39:38 +0530

    9th commit-created is_prime() function

--- primefactor_calc_tdd.py
+++ primefactor_calc_tdd.py
@@ -1,3 +1,8 @@
+def is_prime(PorC):
+    for i in range(2,PorC):
+        if(PorC % i ==0):
+            return False
+    return True
 def find_primes(user_no):
     if(is_prime(user_no)):
         return user_no
