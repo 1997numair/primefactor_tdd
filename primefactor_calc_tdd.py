fa79d493548b8a4d2982ca56766b4c10b2bbe93c
numairnakhwa <40811628+numairnakhwa@users.noreply.github.com>
2021-08-16 19:29:03 +0530

    12th commit-checking product of factors

--- primefactor_calc_tdd.py
+++ primefactor_calc_tdd.py
@@ -1,3 +1,4 @@
+factor_list=[]
 def is_prime(PorC):
     for i in range(2,PorC):
         if(PorC % i ==0):
@@ -5,14 +6,18 @@ def is_prime(PorC):
     return True
 def find_primes(user_no):
     if(is_prime(user_no)):
-        return user_no
-    factor_list=[]
+        factor_list.append(user_no)
+
     for i in range(2,user_no):
-        if(user_no % i==0):
-            factor_list.append(i)
+        if(user_no % i==0 ):
+            if(is_prime(i)):
+                find_primes(i)
+                if(product_of(factor_list)<user_no):
+                    factor_list.append(i)
+            else:
+                find_primes(i)
     return factor_list
 
-_primeno_of=40
-find_primes(_primeno_of)
+_primeno_of=8
 print(find_primes(_primeno_of))
 
