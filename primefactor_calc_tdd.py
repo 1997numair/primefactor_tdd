c8f97ba8be31fe07e3630fa94f05b8057d8f5a63
numairnakhwa <40811628+numairnakhwa@users.noreply.github.com>
2021-08-16 18:42:09 +0530

    10th commit

--- primefactor_calc_tdd.py
+++ primefactor_calc_tdd.py
@@ -6,7 +6,7 @@ def is_prime(PorC):
 def find_primes(user_no):
     if(is_prime(user_no)):
         return user_no
-_primeno_of=3
+_primeno_of=4
 find_primes(_primeno_of)
 print(find_primes(_primeno_of))
 
